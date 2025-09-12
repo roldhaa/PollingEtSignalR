ebe4f8661eb15b34edbe5bc09f531275d069e8d4
PierreOlivierBrillant <pierreolivierbrillant@gmail.com>
2025-09-12 13:11:15 -0400

    Fix imports

--- ngPollingEtSignalR/src/app/polling/polling.component.ts
+++ ngPollingEtSignalR/src/app/polling/polling.component.ts
@@ -1,29 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { UselessTask } from '../models/UselessTask';
 import { HttpClient } from '@angular/common/http';
-import { MatCard, MatCardContent } from '@angular/material/card';
-import { MatCheckbox } from '@angular/material/checkbox';
-import { MatFormField, MatInput, MatInputModule, MatLabel } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
-import { lastValueFrom } from 'rxjs';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-polling',
   standalone: true,
-  imports: [MatCheckbox,MatCardContent,MatCard,MatFormField,FormsModule,MatLabel,CommonModule,MatInput,MatButtonModule],
+  imports: [
+    MatCardModule,
+    MatFormFieldModule,
+    FormsModule,
+    CommonModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCheckboxModule,
+  ],
   templateUrl: './polling.component.html',
-  styleUrls: ['./polling.component.css']
+  styleUrls: ['./polling.component.css'],
 })
 export class PollingComponent implements OnInit {
-  apiUrl = "https://localhost:7289/api/";
+  apiUrl = 'https://localhost:7289/api/';
   title = 'labo.signalr.ng';
   tasks: UselessTask[] = [];
-  taskname: string = "";
+  taskname: string = '';
 
-  constructor(private http:HttpClient){}
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.updateTasks();
@@ -31,13 +38,10 @@ export class PollingComponent implements OnInit {
 
   complete(id: number) {
     // TODO On invoke la méthode pour compléter une tâche sur le serveur (Contrôleur d'API)
-    
   }
 
   addtask() {
     // TODO On invoke la méthode pour ajouter une tâche sur le serveur (Contrôleur d'API)
-
-    
 
     console.log(this.tasks);
   }
@@ -45,6 +49,5 @@ export class PollingComponent implements OnInit {
   async updateTasks() {
     // TODO: Faire une première implémentation simple avec un appel au serveur pour obtenir la liste des tâches
     // TODO: UNE FOIS QUE VOUS AVEZ TESTER AVEC DEUX CLIENTS: Utiliser le polling pour mettre la liste de tasks à jour chaque seconde
-    
   }
 }
--- ngPollingEtSignalR/src/app/signalr/signalr.component.ts
+++ ngPollingEtSignalR/src/app/signalr/signalr.component.ts
@@ -1,49 +1,50 @@
 import { Component, OnInit } from '@angular/core';
-import * as signalR from "@microsoft/signalr";
+import * as signalR from '@microsoft/signalr';
 import { UselessTask } from '../models/UselessTask';
-import { MatCheckbox } from '@angular/material/checkbox';
-import { MatCard, MatCardContent } from '@angular/material/card';
-import { MatFormField, MatInput, MatInputModule, MatLabel } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-signalr',
   standalone: true,
-  imports: [MatCardContent,MatCard,MatFormField,FormsModule,MatLabel,CommonModule,MatInput,MatButtonModule],
+  imports: [
+    MatCardModule,
+    MatFormFieldModule,
+    FormsModule,
+    CommonModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCheckboxModule,
+  ],
   templateUrl: './signalr.component.html',
-  styleUrls: ['./signalr.component.css']
+  styleUrls: ['./signalr.component.css'],
 })
 export class SignalrComponent implements OnInit {
-
   private hubConnection?: signalR.HubConnection;
   usercount = 0;
   tasks: UselessTask[] = [];
-  taskname: string = "";
+  taskname: string = '';
 
   ngOnInit(): void {
-    this.connecttohub()
+    this.connecttohub();
   }
 
   connecttohub() {
     // TODO On doit commencer par créer la connexion vers le Hub
-   
     // TODO On peut commencer à écouter pour les évènements qui vont déclencher des callbacks
-    
     // TODO On doit ensuite se connecter
-    
   }
 
   complete(id: number) {
     // TODO On invoke la méthode pour compléter une tâche sur le serveur
-   
   }
 
   addtask() {
     // TODO On invoke la méthode pour ajouter une tâche sur le serveur
-    
   }
-
 }
